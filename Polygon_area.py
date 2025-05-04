7883b493bd8fd492edb26d6656f45cb6eb1513a9
Aone 1 <arealflash@gmail.com>
2025-05-04 22:03:05 +0330

    change Polygon_area

--- Polygon_area.py
+++ Polygon_area.py
@@ -1,13 +1,19 @@
-from math import tan,pi
+from math import tan, pi
 
 
-def Polygon_area():
-    while True:
-        try:
-            side = int(input("Input the number of side: "))
-            size = float(input("Input the size of side: "))
-            Area = ( side *(size**2)) / (4 * tan(pi/side))
-            return Area
-        except ValueError:
-            print("Input the correct Value")
-print(Polygon_area())
+def Polygon_area(side, size):
+    if side < 3:
+        raise ValueError("A polygon must have at least 3 sides.")
+    if size <= 0:
+        raise ValueError("Side length must be positive.")
+    return (side * (size ** 2)) / (4 * tan(pi / side))
+
+
+# print(Polygon_area())
+try:
+    side = int(input("Input the number of side: "))
+    size = float(input("Input the size of side: "))
+    area = Polygon_area(side, size)
+    print(area)
+except ValueError as e:
+    print("Error", e)
